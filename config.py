40dd796888d638e3abc956ed6d27a4872f841062
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-09-02 02:01:09 +0200

    commit

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 lang = {
     "en": [
-        ["en"],
-        [""]
+        ["en",
+         "Enjoy The Comfort Of Your Holiday Home In The Garbatella District - The Ideal Place To Stay In Rome"
+        ],
+        ["en",
+         "Book Your Next Holiday",
+         "Enjoy The Comfort Of Your Holiday Home In The Garbatella District - The Ideal Place To Stay In Rome"
+        ]
     ],
     "it": [
-        ["it"],
-        [""]
+        ["it",
+         "Goditi Il Comfort Della Tua Casa Vacanza Nel Quartiere Garbatella - Il Luogo Ideale Per Soggiornare A Roma"
+        ],
+        ["it",
+         "Prenota Il Tuo Viaggio",
+         "Goditi Il Comfort Della Tua Casa Vacanza Nel Quartiere Garbatella - Il Luogo Ideale Per Soggiornare A Roma"]
     ]
 }
