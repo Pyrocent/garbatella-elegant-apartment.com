1bcd3cb8ae520fc8b7709b252ceb8a35be5bde89
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-09-04 15:55:32 +0200

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -16,8 +16,15 @@ $(document).ready(function () {
             method: "POST",
             headers: {"Content-Type": "application/x-www-form-urlencoded"},
             body: `lang=${$(this).val().substring(0, 2).toLowerCase()}`,
+        })
+        .then(response => {
+            if (response.status === 200) {
+                window.location.reload();
+            }
+        })
+        .catch(error => {
+            console.error(error);
         });
-        window.location.reload();
     });
 
     $("#book").click(function () {
