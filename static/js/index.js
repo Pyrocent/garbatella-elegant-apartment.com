2b089b87db4e03959e4f2c38993f98d530ceabfc
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-09-04 23:22:42 +0200

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         arrows: false,
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
         slidesToShow: 2,
         slidesToScroll: 2
     });
