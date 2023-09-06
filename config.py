862adbccc66ccf95ae9c4790651ad63d1d838d16
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-09-06 03:15:54 +0200

    commit

--- config.py
+++ config.py
@@ -14,13 +14,14 @@ lang = {
             "A COZY BATHROOM",
             "AN EQUIPPED KITCHEN",
             "TWO SILENT TERRACES",
-            "FREE WIFI",
+            "UNLIMITED FREE WIFI",
             "District",
-            "SUPERMARKET IN FRONT OF",
+            "SUPERMARKET IN FRONT OF THE HOUSE",
             "AUTO PARKING UNDER THE HOUSE",
             "TOBACCO SHOP AT 10M",
-            "CAFE 100M ON SAME STREET",
-            "GARBATELLA METRO 10 MIN WALK"
+            "CAFE AT 100M IN THE SAME STREET",
+            "'GARBATELLA' METRO STATION AT 10 MINUTES WALK",
+            "LESS THAN 25 MINUTES FROM THE CITY CENTRE"
         ],
         [
             "en",
@@ -43,13 +44,14 @@ lang = {
             "UN BAGNO ACCOGLIENTE",
             "UNA CUCINA ACCESSORIATA",
             "DUE TERRAZZI SILENZIOSI",
-            "WIFI GRATUITO",
+            "WIFI GRATUITO ILLIMITATO",
             "Quartiere",
-            "SUPERMERCATO DAVANTI",
+            "SUPERMERCATO DAVANTI CASA",
             "PARCHEGGIO AUTO SOTTO CASA",
             "TABACCHERIA A 10M",
-            "BAR A 100M SULLA STESSA STRADA",
-            "METRO GARBATELLA 1O MIN A PIEDI"
+            "BAR A 100M NELLA STESSA VIA",
+            "STAZIONE METRO 'GARBATELLA' A 1O MINUTI A PIEDI",
+            "A MENO DI 25 MINUTI DAL CENTRO"
         ],
         [
             "it",
