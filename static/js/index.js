f103a2b3aa0eb6a4e45e5aee875dec8685a7a59c
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-09-11 02:09:52 +0200

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+    $("#name").click(function () {
+        window.location.reload();
+    });
+
     $("#languages").change(function () {
         fetch("/", {
             method: "POST",
