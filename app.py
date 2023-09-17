8a98c4ac295c8a4089c4f6a0d6096e00642d451a
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-09-17 23:07:13 +0200

    commit

--- app.py
+++ app.py
@@ -1,58 +1,13 @@
-from secrets import token_hex
-from config import lang
 from flask import (
     Flask,
-    session,
     request,
-    redirect,
-    send_file,
     render_template
 )
 
 app = Flask(__name__)
-app.secret_key = token_hex(16)
-app.template_folder = "templates/min"
 
 @app.get("/")
 def index():
-    return render_template(
-        "index.min.html",
-        lang = lang[session.get("lang", request.accept_languages.best_match(lang.keys(), default = "EN"))]["index"]
-    )
-
-@app.get("/book-holiday-home")
-def book_holiday_home():
-    return render_template(
-        "book-holiday-home.min.html",
-        lang = lang[session.get("lang", request.accept_languages.best_match(lang.keys(), default = "EN"))]["book-holiday-home"]
-    )
-
-@app.get("/tourist-tax-payment")
-def tourist_tax_payment():
-    return render_template(
-        "tourist-tax-payment.min.html",
-        lang = lang[session.get("lang", request.accept_languages.best_match(lang.keys(), default = "EN"))]["tourist-tax-payment"]
-    )
-
-@app.post("/")
-@app.post("/book-holiday-home")
-@app.post("/tourist-tax-payment")
-def _():
-    session["lang"] = request.form.get("lang")
-    print(session.get("lang"))
-    return "", 200
-
-@app.get("/robots.txt")
-def robots():
-    return send_file("robots.txt")
-
-@app.get("/sitemap.xml")
-def sitemaps():
-    return send_file("sitemap.xml")
-
-@app.errorhandler(404)
-@app.errorhandler(405)
-def error(_):
-    return redirect("/")
+    return render_template("en_EN/index.html")
 
 if __name__ == "__main__": app.run(debug = True)
