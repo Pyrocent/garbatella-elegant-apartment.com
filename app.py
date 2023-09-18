77e36191aaf83ff9e961aee45cfcf79026bba590
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-09-18 14:11:56 +0200

    commit

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from secrets import token_hex
+from config import lang
 from flask import (
+    g,
     Flask,
+    session,
     request,
     redirect,
     send_file,
@@ -7,27 +11,38 @@ from flask import (
 )
 
 app = Flask(__name__)
+app.secret_key = token_hex(16)
+app.template_folder = "templates/min"
+
+@app.before_request
+def before_request():
+    g.lang = lang[session.get("lang", request.accept_languages.best_match(lang.keys(), default = "EN"))]["index"]
 
 @app.get("/")
 def index():
-    request.accept_languages.best_match(["de_DE", "en_GB", "en_US", "es_ES", "fr_FR", "it_IT"], default = "en_US")
-    
-    return render_template("index.min.html")
+    return render_template("index.min.html", lang = g.lang)
 
 @app.get("/book-holiday-home")
 def book_holiday_home():
-    return render_template("book-holiday-home.min.html")
+    return render_template("book-holiday-home.min.html", lang = g.lang)
 
 @app.get("/tourist-tax-payment")
 def tourist_tax_payment():
-    return render_template("tourist-tax-payment.min.html")
+    return render_template("tourist-tax-payment.min.html", lang = g.lang)
+
+@app.post("/")
+@app.post("/book-holiday-home")
+@app.post("/tourist-tax-payment")
+def _():
+    session["lang"] = request.form.get("lang")
+    return redirect(request.referrer)
 
 @app.get("/robots.txt")
 def robots():
     return send_file("robots.txt")
 
 @app.get("/sitemap.xml")
-def sitemaps():
+def sitemap():
     return send_file("sitemap.xml")
 
 @app.errorhandler(404)
