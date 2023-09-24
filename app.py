4591b2de8ca4860bba3fc6a5f99b896b8e9a2063
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-09-24 15:35:13 +0200

    commit

--- app.py
+++ app.py
@@ -47,10 +47,16 @@ def change_language():
 @app.post("/disable_days")
 def disable_days():
     days = []
+    only_checkout = None
     for month in Api(randomize = True).get_calendar("940534339344086732")["calendar_months"]:
         for day in month["days"]:
-            if day["available"] == False:
-                days.append(day["date"])
+            if day["available"] == True:
+                only_checkout = True
+            else:
+                if only_checkout == True:
+                    only_checkout = False
+                else:
+                    days.append(day["date"])
 
     return jsonify(days)
 
@@ -68,21 +74,3 @@ def error(_):
     return redirect("/")
 
 if __name__ == "__main__": app.run(debug = True)
-
-# from requests import get
-# from bs4 import BeautifulSoup
-# from datetime import datetime, timedelta
-
-# today = datetime.now()
-# one_year_from_today = today + timedelta(days = 365)
-
-# while today <= one_year_from_today:
-#     page_content = get(f"https://www.airbnb.it/s/Roma/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2023-09-24&monthly_length=3&price_filter_input_type=0&price_filter_num_nights=1&channel=EXPLORE&date_picker_type=calendar&checkout={today.strftime('%Y-%m-%d')}&source=structured_search_input_header&search_type=user_map_move&query=Roma%2C%20RM&place_id=ChIJu46S-ZZhLxMROG5lkwZ3D7k&ne_lat=41.865437039685204&ne_lng=12.495719843478298&sw_lat=41.86453384175038&sw_lng=12.494547739682048&zoom=18.869290362301008&zoom_level=18.869290362301008&search_by_map=true").content
-#     soup = BeautifulSoup(page_content, "html.parser")
-
-#     if soup.find(class_ = "t6mzqp7 dir dir-ltr").get_text() == "Garbatella Elegant Apartament":
-#         print(today.strftime("%Y-%m-%d"), "OKAY")
-#     else:
-#         print(soup.find(class_ = "t6mzqp7 dir dir-ltr").get_text())
-
-#     today += timedelta(days = 1)
